b7eb3588e9374320808274f43a380da7ad6c6dc5
Jon Watson <jbw10@imperial.ac.uk>
2013-03-13 16:10:01 +0000

    we now understand adc

--- exampleApp/Blink.h
+++ exampleApp/Blink.h
@@ -11,14 +11,13 @@ enum {
 };
 
 typedef struct light_state{
-  uint16_t value;
+  int16_t value;
   bool value_set;
   bool neighbour_light;
 } light_state;
 
 typedef struct temp_state{
   int16_t values[TEMP_MAX];
-  float avgs[TEMP_MAX];
   uint8_t index;
   bool value_set;
   bool full;
@@ -31,7 +30,6 @@ void init_light(light_state* l){
 
 void init_temp(temp_state* t){
   memset(t->values,0,TEMP_MAX * sizeof(*t->values));
-  memset(t->avgs,0,TEMP_MAX * sizeof(*t->avgs));
   //hack to start at 0
   t->index = TEMP_MAX - 1;
   t->value_set = FALSE;
@@ -44,19 +42,8 @@ uint16_t latest_temp(temp_state* t){
 
 void temp_push(temp_state* t, uint16_t data){
   //add data
-  uint16_t overwritten;
-  float last_avg = t->avgs[t->index]; 
   t->index = (t->index + 1) % TEMP_MAX; 
-  overwritten = t->values[t->index];
   t->values[t->index] = data;
-
-  //update average
-  if (t->full)
-  {
-    t->avgs[t->index] = last_avg - (float)overwritten/TEMP_MAX + (float)data/TEMP_MAX;
-  } else {
-    t->avgs[t->index] = (last_avg*(t->index - 1) + (float)data)/t->index;
-  }
  
   //update full
   if (t->index == TEMP_MAX - 1)
